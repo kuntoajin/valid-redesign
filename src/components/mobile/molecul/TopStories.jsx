f4ba6c28dea90857156179dd66dd8c2ee643cac3
kuntoajin <kuntoaji.engineer@gmail.com>
2021-11-24 00:29:01 +0700

    test

--- src/components/mobile/molecul/TopStories.jsx
+++ src/components/mobile/molecul/TopStories.jsx
@@ -41,7 +41,7 @@ const TopStories = memo(() => {
                                 <Image 
                                     placeholder="blur"
                                     blurDataURL
-                                    // loader={data => loader(data, list.main_photo)}
+                                    loader={() => list.main_photo}
                                     src={list.main_photo} 
                                     alt={list.title} 
                                     index={index} 
