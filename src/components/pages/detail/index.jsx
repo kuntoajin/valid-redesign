a8502dce685831ed4e09158379956035d321b6ee
kuntoajin <kuntoaji.engineer@gmail.com>
2021-11-26 22:31:08 +0700

    testing twa

--- src/components/pages/detail/index.jsx
+++ src/components/pages/detail/index.jsx
@@ -1,25 +1,43 @@
 import Image from 'next/image'
 import Head from 'next/head'
-import { Box, Container, Text } from "@chakra-ui/react"
+import { Box, Container, Text, HStack, StackDivider } from "@chakra-ui/react"
 import RubrikTitle from "../../atom/RubrikTitle"
 
 const DetailComponents = ({ data }) => {
+    console.log(data?.data)
     return (
         <Container maxW="container.lg">
             <Head>
                 <title>{data?.data?.title}</title>
+                <meta />
             </Head>
             <RubrikTitle title={data?.data?.category?.toUpperCase()} />
             <Box>
+                <HStack divider={<StackDivider orientation="horizontal" borderColor="gray.200" />}>
+                    <Text fontFamily="Montserrat" fontSize="sm">{data?.data?.date}</Text>
+                    <Text fontFamily="Montserrat" fontSize="sm">{data?.data?.time}</Text>
+                </HStack>
                 <Text as="h1" dangerouslySetInnerHTML={{__html: data?.data?.title}} />
                 <Text as="h2" dangerouslySetInnerHTML={{__html: data?.data?.lower_title}} />
+                <Box>
+                    {data?.data?.list_reporter.length <= 0 ? null : <Text as="p" fontSize="xs" fontFamily="Montserrat">Penulis: {data?.data?.list_reporter.map((list, index) => {
+                        return (
+                            list.type === 1 ? <a className='penulis' href={`${list.type === 1 ? `/reporter/${list.id}` : '' }`} key={index}>{list.name}, </a> : 
+                            <small className='penulis' key={index}>{list.name}, </small>
+                        )
+                    })}</Text>}
+                    <Text as="p" fontSize="xs" fontFamily="Montserrat">Editor: {data?.data?.editor}</Text>
+                </Box>
                 <Image 
                     src={data?.data?.main_photo}
+                    alt={data?.data?.title}
                     height={300}
                     width={500}
                     priority
                     quality={5}
                 />
+                <Text dangerouslySetInnerHTML={{__html: data?.data?.caption_photo}}/>
+                <Box dangerouslySetInnerHTML={{__html: data?.data?.content}} />
             </Box>
         </Container>
     )
