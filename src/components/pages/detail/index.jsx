744dc3d9fe67e2fd731b6173a6879c9c5a7163f7
kuntoajin <kuntoaji.engineer@gmail.com>
2021-11-26 18:08:52 +0700

    progress detail

--- src/components/pages/detail/index.jsx
+++ src/components/pages/detail/index.jsx
@@ -1,12 +1,25 @@
-import { Box, Container } from "@chakra-ui/react"
+import Image from 'next/image'
+import Head from 'next/head'
+import { Box, Container, Text } from "@chakra-ui/react"
 import RubrikTitle from "../../atom/RubrikTitle"
 
 const DetailComponents = ({ data }) => {
     return (
         <Container maxW="container.lg">
+            <Head>
+                <title>{data?.data?.title}</title>
+            </Head>
             <RubrikTitle title={data?.data?.category?.toUpperCase()} />
             <Box>
-                test
+                <Text as="h1" dangerouslySetInnerHTML={{__html: data?.data?.title}} />
+                <Text as="h2" dangerouslySetInnerHTML={{__html: data?.data?.lower_title}} />
+                <Image 
+                    src={data?.data?.main_photo}
+                    height={300}
+                    width={500}
+                    priority
+                    quality={5}
+                />
             </Box>
         </Container>
     )
