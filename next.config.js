66c940466c97c6097cd724b85fb7af3c0cb1df66
kuntoajin <kuntoaji.engineer@gmail.com>
2021-11-27 15:18:57 +0700

    monitoring

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
       imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       domains: ['cdn.visiteliti.com'],
-      minimumCacheTTL: 3600,
+      minimumCacheTTL: 60,
     },
   }
