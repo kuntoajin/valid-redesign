0ba8cf8694816be8a4ed4790aa9519aa9f4ec8ae
kuntoajin <kuntoaji.engineer@gmail.com>
2021-11-30 14:56:32 +0700

    fix error

--- src/components/molecul/kanal/Showcase/atoms/image.jsx
+++ src/components/molecul/kanal/Showcase/atoms/image.jsx
@@ -3,22 +3,24 @@ import Image from 'next/image'
 import { Box, Text } from "@chakra-ui/react";
 
 const ImageComponent = memo(({data}) => {
+    const photo = data?.largeYard?.main_photo
+
     return (
         <Box p={{md: "10px", sm: "0"}} pt={0}>
             {
-                data && 
+                photo && 
                 <Image 
                     placeholder="blur"
                     blurDataURL
-                    src={data?.main_photo}
-                    alt={data?.title}
+                    src={photo}
+                    alt={data?.largeYard?.title}
                     priority
                     quality={3}
                     width={500}
                     height={300}
                 />
             }
-            <Text as="em" dangerouslySetInnerHTML={{__html: data?.caption_photo}} fontSize="xs" />
+            <Text as="em" dangerouslySetInnerHTML={{__html: data?.largeYard?.caption_photo}} fontSize="xs" />
         </Box>
     )
 })
--- src/components/molecul/kanal/Showcase/atoms/info.jsx
+++ src/components/molecul/kanal/Showcase/atoms/info.jsx
@@ -6,13 +6,13 @@ const InfoComponent = memo(({data, title}) => {
     return (
         <Box>
             <Text as="h1" fontSize="30px" fontWeight="700" color="orange">{title}</Text>
-            <RubrikTitle title={data?.sub_kanal.toUpperCase()} />
+            <RubrikTitle title={data?.largeYard?.sub_kanal?.toUpperCase()} />
             <HStack divider={<StackDivider orientation="horizontal" borderColor="gray.200" />}>
-                <Text fontFamily="Montserrat" fontSize="sm">{data?.date}</Text>
-                <Text fontFamily="Montserrat" fontSize="sm">{data?.time}</Text>
+                <Text fontFamily="Montserrat" fontSize="sm">{data?.largeYard?.date}</Text>
+                <Text fontFamily="Montserrat" fontSize="sm">{data?.largeYard?.time}</Text>
             </HStack>
-            <Text as="h2" fontSize="22px">{data?.title}</Text>
-            <Text as="p">{data?.lower_title}</Text>
+            <Text as="h2" fontSize="22px">{data?.largeYard?.title}</Text>
+            <Text as="p">{data?.largeYard?.lower_title}</Text>
         </Box>
     )
 })
