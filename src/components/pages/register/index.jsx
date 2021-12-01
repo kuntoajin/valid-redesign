e786e9d79cb7376d0b9db87b4c6314a0447fa22a
kuntoajin <kuntoaji.engineer@gmail.com>
2021-12-01 07:41:19 +0700

    monitoring

---
+++ src/components/pages/register/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@chakra-ui/react"
+
+const RegisterPage = () => {
+    return(
+        <Container maxW="container.lg">
+
+        </Container>
+    )
+}
+
+export default RegisterPage
