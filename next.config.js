52c1ec4be7bd95a7d87f6a0b93e26f8da23197f2
kuntoajin <kuntoaji.engineer@gmail.com>
2021-12-02 12:40:01 +0700

    pwa monitoring

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
+  reactStrictMode: true,
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
+    runtimeCaching,
+    // buildExcludes: [/middleware-manifest.json$/],
     // disable: process.env.NODE_ENV === "development",
   },
   images: {
