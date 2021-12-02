52ddc2f37da4789c3fbc0b5dabc6344a51aa034f
kuntoajin <kuntoaji.engineer@gmail.com>
2021-12-03 00:03:31 +0700

    fix pwa setting

--- next.config.js
+++ next.config.js
@@ -2,14 +2,9 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-  reactStrictMode: true,
   pwa: {
     dest: "public",
-    register: true,
-    skipWaiting: true,
     runtimeCaching,
-    // buildExcludes: [/middleware-manifest.json$/],
-    // disable: process.env.NODE_ENV === "development",
   },
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
